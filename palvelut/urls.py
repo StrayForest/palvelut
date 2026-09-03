0e8df134a61cfdcaddb4a7255dfda510c5bf780e
Aleksei Lisitcin <157023683+StrayForest@users.noreply.github.com>
2026-09-03 07:44:44 +0300

    P0: add mount root redirect

--- palvelut/urls.py
+++ palvelut/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from palvelut.views import localized_home
+from palvelut.views import localized_home, public_mount_root
 
 urlpatterns = [
+    path("palvelut/", public_mount_root, name="public-mount-root"),
     path("palvelut/<str:locale>/", localized_home, name="localized-home"),
 ]
