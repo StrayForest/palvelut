27f1f838c7f2bf509d0ccf64820ee700eaddbd84
Aleksei Lisitcin <157023683+StrayForest@users.noreply.github.com>
2026-09-03 07:44:51 +0300

    P0: render canonical and hreflang metadata

--- palvelut/views.py
+++ palvelut/views.py
@@ -1,13 +1,34 @@
 from django.conf import settings
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.utils import translation
 
 SUPPORTED_LOCALES = {code for code, _name in settings.LANGUAGES}
 
 
+def public_mount_root(request):
+    return redirect("localized-home", locale=settings.LANGUAGE_CODE)
+
+
 def localized_home(request, locale: str):
     if locale not in SUPPORTED_LOCALES:
         raise Http404("Unsupported locale")
+
+    canonical_url = f"{settings.PUBLIC_BASE_URL}/{locale}/"
+    hreflang_links = [
+        (code, f"{settings.PUBLIC_BASE_URL}/{code}/")
+        for code, _name in settings.LANGUAGES
+    ]
+    x_default_url = f"{settings.PUBLIC_BASE_URL}/{settings.LANGUAGE_CODE}/"
+
     with translation.override(locale):
-        return render(request, "home.html", {"locale": locale})
+        return render(
+            request,
+            "home.html",
+            {
+                "locale": locale,
+                "canonical_url": canonical_url,
+                "hreflang_links": hreflang_links,
+                "x_default_url": x_default_url,
+            },
+        )
