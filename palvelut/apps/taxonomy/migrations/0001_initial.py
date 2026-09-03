5c38921f786542550566f47df32e1602100dfbd6
Aleksei Lisitcin <157023683+StrayForest@users.noreply.github.com>
2026-09-03 21:09:10 +0300

    P1: type taxonomy migration dependencies

--- palvelut/apps/taxonomy/migrations/0001_initial.py
+++ palvelut/apps/taxonomy/migrations/0001_initial.py
@@ -5,7 +5,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
+    dependencies: list[tuple[str, str]] = []
 
     operations = [
         migrations.CreateModel(
